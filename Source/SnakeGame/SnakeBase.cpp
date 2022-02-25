03deb0274dc9fd34634346fea8460b13e5221dfb
kirillkychkin <kychkin.kirill@gmail.com>
2022-02-25 16:13:05 +0900

    add method AddSnakeElement call AddSnakeElement(4) on BeginPlay

--- Source/SnakeGame/SnakeBase.cpp
+++ Source/SnakeGame/SnakeBase.cpp
@@ -9,14 +9,14 @@ ASnakeBase::ASnakeBase()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	ElementSize = 100.f;
 }
 
 // Called when the game starts or when spawned
 void ASnakeBase::BeginPlay()
 {
 	Super::BeginPlay();
-	GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, GetActorTransform());
+	AddSnakeElement(4);
 }
 
 // Called every frame
@@ -24,5 +24,16 @@ void ASnakeBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ASnakeBase::AddSnakeElement(int ElementsNum)
+{
+	for (int i = 0; i < ElementsNum; ++i)
+	{
+		FVector NewLocation(SnakeElements.Num() * ElementSize, 0, 0);
+		FTransform NewTransform = FTransform(NewLocation);
+		auto NewSnakeElement = GetWorld()->SpawnActor<ASnakeElementBase>(SnakeElementClass, NewTransform);
+		SnakeElements.Add(NewSnakeElement);
+	}
 }
 
