857d121599bbd4d6dd7f5ba0665a1705e8a90e37
kirillkychkin <kychkin.kirill@gmail.com>
2022-02-25 15:51:56 +0900

    add snake and snake elem cpp classes

--- Source/SnakeGame/PlayerPawnBase.cpp
+++ Source/SnakeGame/PlayerPawnBase.cpp
@@ -3,6 +3,7 @@
 
 #include "PlayerPawnBase.h"
 #include <Engine/Classes/Camera/CameraComponent.h>
+#include "SnakeBase.h"
 
 // Sets default values
 APlayerPawnBase::APlayerPawnBase()
@@ -18,7 +19,8 @@ APlayerPawnBase::APlayerPawnBase()
 void APlayerPawnBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	SetActorRotation(FRotator(-90, 0, 0));
+	CreateSnakeActor();
 }
 
 // Called every frame
@@ -33,5 +35,10 @@ void APlayerPawnBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void APlayerPawnBase::CreateSnakeActor()
+{
+	SnakeActor = GetWorld()->SpawnActor<ASnakeBase>(SnakeActorClass, FTransform());
 }
 
